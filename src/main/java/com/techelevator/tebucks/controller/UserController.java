c8a66f958e79cd613d104f8c26662a2805a62e43
e-fanto <elise.m.fanto@gmail.com>
2023-07-05 19:30:20 -0400

    lots of packages and classes added. more to come. Also, the database table for the transactions should contain the user to and user from as a FK from the account ID

---
+++ src/main/java/com/techelevator/tebucks/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.techelevator.tebucks.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    //    @GetMapping(path = "/api/users")
+
+}
---
+++ src/main/java/com/techelevator/tebucks/model/Transaction.java
@@ -0,0 +1,82 @@
+package com.techelevator.tebucks.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import java.text.DecimalFormat;
+
+public class Transaction {
+    @NotBlank
+    private int transferId;
+    @NotBlank
+    private String transferType;
+    @NotBlank
+    private String transferStatus;
+    @NotBlank
+    private int userFrom;
+    @NotBlank
+    private int userTo;
+    @Positive
+    private double amount;
+    @NotBlank
+    private int userId;
+
+    private static DecimalFormat decimalTo100th = new DecimalFormat("0.00");
+
+    public Transaction(){    }
+
+    public int getTransferId() {
+        return transferId;
+    }
+
+    public void setTransferId(int transferId) {
+        this.transferId = transferId;
+    }
+
+    public String getTransferType() {
+        return transferType;
+    }
+
+    public void setTransferType(String transferType) {
+        this.transferType = transferType;
+    }
+
+    public String getTransferStatus() {
+        return transferStatus;
+    }
+
+    public void setTransferStatus(String transferStatus) {
+        this.transferStatus = transferStatus;
+    }
+
+    public int getUserFrom() {
+        return userFrom;
+    }
+
+    public void setUserFrom(int userFrom) {
+        this.userFrom = userFrom;
+    }
+
+    public int getUserTo() {
+        return userTo;
+    }
+
+    public void setUserTo(int userTo) {
+        this.userTo = userTo;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
