e4a74b15bd1f52bdd93cab4b7e6112a2f0ff1b75
e-fanto <elise.m.fanto@gmail.com>
2023-07-05 20:40:12 -0400

    some updates

--- src/main/java/com/techelevator/tebucks/dao/AccountDao.java
+++ src/main/java/com/techelevator/tebucks/dao/AccountDao.java
@@ -1,4 +1,15 @@
 package com.techelevator.tebucks.dao;
 
 public interface AccountDao {
+//     private final
+
+//    Account makeNewAccount
+
+//
+//    }
+
+//    public List<Transfer> get
+
+
+
 }
