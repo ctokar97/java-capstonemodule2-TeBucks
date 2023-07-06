69ea6ced4eead805d313fb71e4496acec9dce415
e-fanto <elise.m.fanto@gmail.com>
2023-07-05 20:48:05 -0400

    ...

--- src/main/java/com/techelevator/tebucks/dao/AccountDao.java
+++ src/main/java/com/techelevator/tebucks/dao/AccountDao.java
@@ -1,9 +1,10 @@
 package com.techelevator.tebucks.dao;
 
 public interface AccountDao {
-//     private final
+    private final double STARTING_BALANCE = 1000.00;
 
-//    Account makeNewAccount
+    Account makeNewAccount(){
+
 
 //
 //    }
