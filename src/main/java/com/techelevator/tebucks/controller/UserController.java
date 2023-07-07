840fdd70e2da4d26cc98c4cf83f737cd16e4545a
e-fanto <elise.m.fanto@gmail.com>
2023-07-07 09:22:07 -0400

    mostly attempting to create account in user controller

--- src/main/java/com/techelevator/tebucks/controller/UserController.java
+++ src/main/java/com/techelevator/tebucks/controller/UserController.java
@@ -3,6 +3,7 @@ package com.techelevator.tebucks.controller;
 import com.techelevator.tebucks.dao.AccountDao;
 import com.techelevator.tebucks.dao.TransferDao;
 import com.techelevator.tebucks.model.Transfer;
+import com.techelevator.tebucks.dao.AccountDao;
 import com.techelevator.tebucks.security.dao.UserDao;
 import com.techelevator.tebucks.security.model.RegisterUserDto;
 import com.techelevator.tebucks.security.model.User;
@@ -10,7 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 
