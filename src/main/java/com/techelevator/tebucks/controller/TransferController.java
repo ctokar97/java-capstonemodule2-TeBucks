e72a6a618b539e0b36124688f12f311a150376ed
Christopher Tokar <ctokar97@gmail.com>
2023-07-07 17:20:42 -0400

    Another update to transfer controller for create method

--- src/main/java/com/techelevator/tebucks/controller/TransferController.java
+++ src/main/java/com/techelevator/tebucks/controller/TransferController.java
@@ -52,72 +52,94 @@ public class TransferController {
         return transferDao.getTransferbyId(transferId);//get transfer by id
     }
 //sender account balance, transfer amount, sender's ID, transfer username
-@ResponseStatus(HttpStatus.CREATED)
-@PostMapping(path = "/api/transfers")
-public Transfer createTransfer(@RequestBody NewTransferDto transferDto, Principal principal){
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(path = "/api/transfers")
+    public Transfer createTransfer(@RequestBody NewTransferDto transferDto, Principal principal){
+        int thisUserId = userDao.getUserByUsername(principal.getName()).getId();
+        int userToId = transferDto.getUserTo();
+        double userFromBalance = accountDao.getBalanceByAccountId(thisUserId);
+        double userToBalance = accountDao.getBalanceByAccountId(transferDto.getUserTo());
 
+        if (transferDto.getUserFrom() == transferDto.getUserTo()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Transfer must be between 2 unique accounts");
+        }
+        if (transferDto.getAmount() <= 0)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount must be greater than 0.");
 
-    if (transferDto.getUserFrom() == transferDto.getUserTo()){
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Transfer must be between 2 unique accounts");
+        if(transferDto.getTransferType().equals("Request")){
+            Transfer transfer = new Transfer();
+
+            transfer.setTransferType(transferDto.getTransferType());
+            transfer.setUserTo(transferDto.getUserTo());
+            transfer.setUserFrom(transferDto.getUserFrom());
+            transfer.setAmount(transferDto.getAmount());
+            transfer.setTransferStatus("Pending");
+
+            return transferDao.createTransfer(transfer);
+
+        }
+        ///SENDING MONEY
+        // - USER balance > s.amount
+        // - Take money after send happens - USER_From new balance = user_from.balance - s.amount
+        // - user.to balance = user.to balance + s.amount
+
+        if (transferDto.getTransferType().equals("Send")){
+            Transfer transfer = new Transfer();
+
+            if (userFromBalance <= transferDto.getAmount()){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Balance is not big enough to send that amount");
+            }
+
+            userFromBalance = userFromBalance - transferDto.getAmount();
+            userToBalance = userToBalance + transferDto.getAmount();
+
+            transfer.setTransferType(transferDto.getTransferType());
+            transfer.setUserTo(transferDto.getUserTo());
+            transfer.setUserFrom(transferDto.getUserFrom());
+            transfer.setAmount(transferDto.getAmount());
+            transfer.setTransferStatus("Approved");
+
+            return transferDao.createTransfer(transfer);
+        }
+        return null;
+
     }
-    if (transferDto.getAmount() <= 0)
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Amount must be greater than 0.");
+    //in this we need amount, transferType, userFrom, userTo
+    //check for it is between unique accounts
+    //check transfer amount is greater than 0
+    //after this set TransferStatus to pending
+    //code for if status is approved and compare to service that it does not exceed 1000.00
+    //if amount is fine create the transfer and update the transfer
+    //create report for tears if transfer would overdraw account
+    //return transfer
+    //else set transferId create transfer and return good transfer
 
-    if(transferDto.getTransferType().equals("Request")){
-        Transfer transfer = new Transfer();
 
-        transfer.setTransferType(transferDto.getTransferType());
-        transfer.setUserTo(transferDto.getUserTo());
-        transfer.setUserFrom(transferDto.getUserFrom());
-        transfer.setAmount(transferDto.getAmount());
-        transfer.setTransferStatus("Pending");
+    @PutMapping(path = "/api/transfers/{id}/status")
+    public Transfer updateTransfer(@PathVariable int transferId, @RequestBody TransferStatusUpdateDto transferStatusUpdateDto, Principal principal){
+    Transfer transfer = transferDao.getTransferbyId(transferId);
+    //check you are not person requesting or request is going to
+    //if (principal.getName().equals(transfer.getUserFrom());
+    //thorw forbidden
+    //check request if incoming transfer type is a request and incoming transfer status equals rejected
 
-        return transferDao.createTransfer(transfer);
+        //else if
 
-    }
-    if (transferDto.getTransferType().equals("Send")){
+        //approve request
+        //then check if enough money
+        //then get transfer
 
-    }
+
+
+
 
     return null;
+        }
 
-}
-//in this we need amount, transferType, userFrom, userTo
-//check for it is between unique accounts
-//check transfer amount is greater than 0
-//after this set TransferStatus to pending
-//code for if status is approved and compare to service that it does not exceed 1000.00
-//if amount is fine create the transfer and update the transfer
-//create report for tears if transfer would overdraw account
-//return transfer
-//else set transferId create transfer and return good transfer
-
-
-@PutMapping(path = "/api/transfers/{id}/status")
-public Transfer updateTransfer(@PathVariable int transferId, @RequestBody TransferStatusUpdateDto transferStatusUpdateDto, Principal principal){
-Transfer transfer = transferDao.getTransferbyId(transferId);
-//check you are not person requesting or request is going to
-//if (principal.getName().equals(transfer.getUserFrom());
-//thorw forbidden
-//check request if incoming transfer type is a request and incoming transfer status equals rejected
-
-    //else if
-
-    //approve request
-    //then check if enough money
-    //then get transfer
-
-
-
-
-
-return null;
-    }
-
-//in this we will need to be able to get a transferId and set a transferStatus to that id
-//we will need to see if it is approved
-//we will need to get balance to see if update is valid and report to log if not
-//account for overdrawn account
-//return the transfer if valid
+    //in this we will need to be able to get a transferId and set a transferStatus to that id
+    //we will need to see if it is approved
+    //we will need to get balance to see if update is valid and report to log if not
+    //account for overdrawn account
+    //return the transfer if valid
 
 }
